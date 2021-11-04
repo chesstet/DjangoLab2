ccf4adc553060233b1bc7d5935f9e460dc5c79e7
infShame <fjehhur@gmail.com>
2021-11-04 15:12:00 +0200

    added crud

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from peewee import *
-from postgreSQL import *
+import postgreSQL
+from postgreSQL import PostgreSQLStudent
 import CRUD
 
 if __name__ == '__main__':
     #"id", "name", "grade", "speciality", "course", "cohort", "discipline"
-    with pg_db:
-        pass
-        #pg_db.create_tables([PostgreSQLStudent])
+    # with db:
+    #     pg_db.create_tables([PostgreSQLStudent])
 
-    testdata = ['Good Student', 80, 123, 3, 'IV-91', 'Sport']
+    testdata = ['Test Student', 75, 126, 3, 'IK-12', 'Programming']
 
-    with pg_db:
-        CRUD.create(PostgreSQLStudent, testdata)
+    print(CRUD.executeCommand(CRUD.update, PostgreSQLStudent, postgreSQL.pg_db, testdata))
